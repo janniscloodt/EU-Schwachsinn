6f9a8540806068aa483ef02a9b4e942bb084c5b2
janniscloodt <janniscloodt@outlook.de>
2025-10-08 17:39:10 +0200

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react(),tailwindcss(),],
-    base: '/EU-Schwachsinn/',
+    base: '/EU-Verordnungskompatibler-Namensgenerator/',
     build: { outDir: 'build' }
 })
